f38efe64e6112378778854654fa1c8c8ba7b575f
Eugenio di Lorenzo <gege92@teletu.it>
2017-11-23 09:36:38 +0100

    Initial Commit

---
+++ PDPReatingAndReviewExpandebleLabelTests/PDPReatingAndReviewExpandebleLabelTests.swift
@@ -0,0 +1,36 @@
+//
+//  PDPReatingAndReviewExpandebleLabelTests.swift
+//  PDPReatingAndReviewExpandebleLabelTests
+//
+//  Created by Wind3 on 23/11/2017.
+//  Copyright © 2017 Wind3. All rights reserved.
+//
+
+import XCTest
+@testable import PDPReatingAndReviewExpandebleLabel
+
+class PDPReatingAndReviewExpandebleLabelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
