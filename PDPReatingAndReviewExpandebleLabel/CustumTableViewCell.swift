ed3972a3a31ab5411930119a827a7c666c3a203c
Eugenio di Lorenzo <gege92@teletu.it>
2017-11-23 09:47:52 +0100

    initial commit

---
+++ PDPReatingAndReviewExpandebleLabel/CustumTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustumTableViewCell.swift
+//  PDPReatingAndReviewExpandebleLabel
+//
+//  Created by Wind3 on 23/11/2017.
+//  Copyright © 2017 Wind3. All rights reserved.
+//
+
+import UIKit
+
+class CustumTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var view:UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PDPReatingAndReviewExpandebleLabel/ViewController.swift
+++ PDPReatingAndReviewExpandebleLabel/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustumTableViewCell
+        
+        return cell
+    }
 
+    
 }
 
